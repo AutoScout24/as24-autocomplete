4c43e49897e9c3cdeb0bf4cfa222c08f19538629
Stefan Retta <stefan@retta.de>
2017-07-27 13:14:34 +0200

    Ignore whitespaces when matching suggestions vs user input to avoid problems with indented values.

--- src/data-sources/plain-data-source.js
+++ src/data-sources/plain-data-source.js
@@ -17,18 +17,20 @@ class Suggestion {
     }
 }
 
+const trimString = (str) => str.split(String.fromCharCode(92)).join('') // remove all backslashes
+        .split('&nbsp').join('')
+        .trim();
+
 /**
  * Test the string against item's value\
  * @param {RegExp} regexp
  * @returns {function}
  */
-const valuePredicate = (regexp) =>
+const matchValuePredicate = (regexp) =>
     /**
      * @param {Suggestion} item
      */
-    item =>
-        item.value.match(regexp) !== null;
-
+    item => trimString(item.value).match(regexp) !== null;
 
 /**
  * @class
@@ -41,12 +43,19 @@ class PlainDataSource extends HTMLElement {
      */
     fetchItems(queryString) {
         return new Promise(res => {
+            const trimmedQueryString = trimString(queryString);
             const keyVals = this.extractKeyValues();
+
+            const startPredicate = matchValuePredicate(new RegExp(`^${trimmedQueryString}`, 'ig'));
+            const restPredicate = matchValuePredicate(new RegExp(`${trimmedQueryString}`, 'ig'));
+
             const startingWith = keyVals
-                .filter(valuePredicate(new RegExp(`^${queryString}`, 'ig')));
+                .filter(x => startPredicate(x));
+
             const theRestContaining = keyVals
-                .filter(x => startingWith.indexOf(x) === -1)
-                .filter(valuePredicate(new RegExp(`${queryString}`, 'ig')));
+                    .filter(x => startingWith.indexOf(x) === -1)
+                    .filter(x => restPredicate(x));
+
             return res(startingWith.concat(theRestContaining));
         });
     }
